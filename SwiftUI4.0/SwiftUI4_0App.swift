ed58ab2ab5a3c1f3fabb5a4cf89277e7258d0d47
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-11-26 16:54:41 +0530

    Initial Commit

---
+++ SwiftUI4.0/SwiftUI4_0App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI4_0App.swift
+//  SwiftUI4.0
+//
+//  Created by Adarsh Shukla on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI4_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
